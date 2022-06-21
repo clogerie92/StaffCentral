59a279dab2ba1cd01943d65740ecd00345d3fe80
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-21 18:03:51 -0400

    created employee database with tables, added data to seed tables with

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Operations"), ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Front End Engineer", 80000, 2), ("Back End Engineer", 80000, 2), ("Sales Representative", 90000, 1), ("Operations Manager", 75000, 3), ("CPA", 120000, 4), ("Tech Lead", 100000, 2), ("Senior Sales Representative", 120000, 1);
+
+
