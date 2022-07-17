23a76ea34e4582577906c92d6bd8a222d0d7d963
clogerie <66565287+clogerie@users.noreply.github.com>
2022-07-17 15:41:38 -0400

    added foreign keys to tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,10 @@ CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -19,5 +22,11 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL
 );
